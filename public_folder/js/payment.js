3561bf378c0530f84a0c5f921ef3cfc70d7c0841
yasir <ynaveed@gmail.com>
2015-06-13 21:14:34 +0200

    fix the form

--- public_folder/js/payment.js
+++ public_folder/js/payment.js
@@ -1,21 +1,28 @@
 $(document).ready(function() {
+
   $("#payment-form").submit(function(event) {
-    event.preventDefault();
     // Deactivate submit button to avoid further clicks
-    $('.submit-button').attr("disabled", "disabled");
-
-    paymill.createToken({
-      number: $('.card-number').val(),  // required, ohne Leerzeichen und Bindestriche
-      exp_month: $('.card-expiry-month').val(),   // required
-      exp_year: $('.card-expiry-year').val(),     // required, vierstellig z.B. "2016"
-      cvc: $('.card-cvc').val(),                  // required
-      amount_int: $('.card-amount-int').val(),    // required, integer, z.B. "15" fÃ¼r 0,15 Euro
-      currency: $('.card-currency').val(),    // required, ISO 4217 z.B. "EUR" od. "GBP"
-      cardholder: $('.card-holdername').val() // optional
-    }, PaymillResponseHandler);                   // Info dazu weiter unten
-
-    return false;
+    console.log('submitted');
   });
+
+
+  $('.submit-button').on('click',function(){
+
+    getToken();
+  });
+
+  function getToken(){
+      paymill.createToken({
+        number: $('.card-number').val(),  // required, ohne Leerzeichen und Bindestriche
+        exp_month: $('.card-expiry-month').val(),   // required
+        exp_year: $('.card-expiry-year').val(),     // required, vierstellig z.B. "2016"
+        cvc: $('.card-cvc').val(),                  // required
+        amount_int: $('.card-amount-int').val(),    // required, integer, z.B. "15" fÃ¼r 0,15 Euro
+        currency: $('.card-currency').val(),    // required, ISO 4217 z.B. "EUR" od. "GBP"
+        cardholder: $('.card-holdername').val() // optional
+      }, PaymillResponseHandler); 
+  }
+
 
   function PaymillResponseHandler(error, result) {
     if (error) {
@@ -30,6 +37,8 @@ $(document).ready(function() {
       // Insert token into form in order to submit to server
       form.append("");
       $("#token").val(token);
+
+      $('#payment-form').submit();
       console.log(result);
       console.log(token);
     }
