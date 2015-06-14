63d57b33e48add958c70edf618a9aa901c9f9c56
dan <warmspringwinds@gmail.com>
2015-06-14 11:13:34 +0200

    cleaned up

--- create_db.py
+++ create_db.py
@@ -5,11 +5,18 @@ db.drop_all()
 db.create_all()
 
 new_user = User(username='Daniil Pakhomov', email='warmspringwinds@gmail.com',
-                password='123')
+                password='123', stars=5)
 
-new_transaction = Transaction(paymill_id='tran_e6a03da2f67eb54c3e9cd45f4c7f', amount=4200, currency='EUR', user_id=1)
+new_user_2 = User(username='Areeb', email='areeb@gmail.com', password='100', stars=4)
+
+new_transaction = Transaction(paymill_id='tran_e6a03da2f67eb54c3e9cd45f4c7f',
+                              amount=4200,
+                              currency='EUR',
+                              user_id=1,
+                              receiver_id=2)
 
 
 db.session.add(new_user)
+db.session.add(new_user_2)
 db.session.add(new_transaction)
 db.session.commit()
